4ac2acd8e42334240efde451c4ac9dcbf8093c70
Vikas <vkq0011@gmail.com>
2017-08-23 15:59:48 +0530

    add player activity for android TV

---
+++ app/src/main/java/com/vdocipher/sampleapp/tv/PlaybackOverlayFragment.java
@@ -0,0 +1,147 @@
+package com.vdocipher.sampleapp.tv;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v17.leanback.app.PlaybackFragment;
+import android.support.v17.leanback.widget.AbstractDetailsDescriptionPresenter;
+import android.support.v17.leanback.widget.Action;
+import android.support.v17.leanback.widget.ArrayObjectAdapter;
+import android.support.v17.leanback.widget.ClassPresenterSelector;
+import android.support.v17.leanback.widget.ControlButtonPresenterSelector;
+import android.support.v17.leanback.widget.ListRow;
+import android.support.v17.leanback.widget.ListRowPresenter;
+import android.support.v17.leanback.widget.OnActionClickedListener;
+import android.support.v17.leanback.widget.PlaybackControlsRow;
+import android.support.v17.leanback.widget.PlaybackControlsRowPresenter;
+import android.util.Log;
+
+import com.vdocipher.aegis.media.MediaInfo;
+
+/**
+ * Handles the UI for playback controls and state.
+ *
+ * The UI is updated by calls from the host activity when it receives appropriate callbacks from
+ * the VdoPlayer.
+ */
+
+public class PlaybackOverlayFragment extends PlaybackFragment {
+    private static final String TAG = PlaybackOverlayFragment.class.getSimpleName();
+
+    private MediaInfo mMediaInfo;
+    private PlaybackControlsRow mPlaybackControlsRow;
+    private ArrayObjectAdapter mPrimaryActionsAdapter;
+    private int mVdoPlayerState;
+
+    private PlaybackControlsRow.PlayPauseAction mPlayPauseAction;
+    private PlaybackControlsRow.FastForwardAction mFastForwardAction;
+    private PlaybackControlsRow.RewindAction mRewindAction;
+
+    private ArrayObjectAdapter mRowsAdapter;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+
+        setBackgroundType(PlaybackOverlayFragment.BG_LIGHT);
+        setFadingEnabled(true);
+
+        setUpRows();
+    }
+
+    // called when state change callback is received from VdoPlayer
+    public void playbackStateChanged(boolean playWhenReady, int state) {
+        mVdoPlayerState = state;
+        if (playWhenReady) {
+            setFadingEnabled(true);
+            mPlayPauseAction.setIndex(PlaybackControlsRow.PlayPauseAction.PAUSE);
+            mPlayPauseAction.setIcon(mPlayPauseAction.getDrawable(PlaybackControlsRow.PlayPauseAction.PAUSE));
+        } else {
+            //setFadingEnabled(false);
+            mPlayPauseAction.setIndex(PlaybackControlsRow.PlayPauseAction.PLAY);
+            mPlayPauseAction.setIcon(mPlayPauseAction.getDrawable(PlaybackControlsRow.PlayPauseAction.PLAY));
+        }
+        notifyPlaybackRowChanged();
+    }
+
+    public void playbackPositionChanged(long positionMs) {
+        mPlaybackControlsRow.setCurrentTimeLong(positionMs);
+    }
+
+    public void playbackDurationChanged(long durationMs) {
+        mPlaybackControlsRow.setTotalTimeLong(durationMs);
+    }
+
+    private void setUpRows() {
+        ClassPresenterSelector ps = new ClassPresenterSelector();
+
+        PlaybackControlsRowPresenter playbackControlsRowPresenter =
+                new PlaybackControlsRowPresenter();
+
+        ps.addClassPresenter(PlaybackControlsRow.class, playbackControlsRowPresenter);
+        ps.addClassPresenter(ListRow.class, new ListRowPresenter());
+        mRowsAdapter = new ArrayObjectAdapter(ps);
+
+        // add as first Row of mRowsAdapter
+        addPlaybackControlsRow();
+
+        // set action click listener
+        playbackControlsRowPresenter.setOnActionClickedListener(new OnActionClickedListener() {
+            public void onActionClicked(Action action) {
+                if (action.getId() == mPlayPauseAction.getId()) {
+                    togglePlayback(mPlayPauseAction.getIndex() == PlaybackControlsRow.PlayPauseAction.PLAY);
+                } else if (action.getId() == mFastForwardAction.getId()) {
+                    fastForward();
+                } else if (action.getId() == mRewindAction.getId()) {
+                    rewind();
+                }
+            }
+        });
+        setAdapter(mRowsAdapter);
+    }
+
+    private void addPlaybackControlsRow() {
+        mPlaybackControlsRow = new PlaybackControlsRow();
+        mRowsAdapter.add(mPlaybackControlsRow);
+
+        ControlButtonPresenterSelector presenterSelector = new ControlButtonPresenterSelector();
+        mPrimaryActionsAdapter = new ArrayObjectAdapter(presenterSelector);
+        mPlaybackControlsRow.setPrimaryActionsAdapter(mPrimaryActionsAdapter);
+
+        Activity activity = getActivity();
+        mPlayPauseAction = new PlaybackControlsRow.PlayPauseAction(activity);
+        mFastForwardAction = new PlaybackControlsRow.FastForwardAction(activity);
+        mRewindAction = new PlaybackControlsRow.RewindAction(activity);
+
+        // PrimaryAction setting
+        mPrimaryActionsAdapter.add(mPlayPauseAction);
+        mPrimaryActionsAdapter.add(mFastForwardAction);
+        mPrimaryActionsAdapter.add(mRewindAction);
+    }
+
+    private void togglePlayback(boolean playWhenReady) {
+        ((TvPlayerActivity)getActivity()).setPlayWhenReady(playWhenReady);
+    }
+
+    private void fastForward() {
+        ((TvPlayerActivity)getActivity()).fastForward();
+    }
+
+    private void rewind() {
+        ((TvPlayerActivity)getActivity()).rewind();
+    }
+
+    // todo to be used for displaying optional details above primary action row
+    private static class DetailsDescriptionPresenter extends AbstractDetailsDescriptionPresenter {
+
+        @Override
+        protected void onBindDescription(ViewHolder viewHolder, Object item) {
+            MediaInfo mediaInfo = (MediaInfo) item;
+            if (mediaInfo != null) {
+                viewHolder.getTitle().setText(mediaInfo.title);
+                viewHolder.getBody().setText(mediaInfo.description);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/vdocipher/sampleapp/tv/TvPlayerActivity.java
@@ -0,0 +1,140 @@
+package com.vdocipher.sampleapp.tv;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.vdocipher.aegis.media.ErrorDescription;
+import com.vdocipher.aegis.media.Track;
+import com.vdocipher.aegis.player.VdoPlayer;
+import com.vdocipher.aegis.player.VdoPlayerFragment;
+import com.vdocipher.sampleapp.R;
+
+/**
+ * This class serves as a basic example of integrating playback in android TV.
+ *
+ * It uses a class extending {@link android.support.v17.leanback.app.PlaybackFragment} to provide a
+ * standard playback control ui for android TV. However, you can make your own or use a different
+ * playback controller layout.
+ *
+ * The {@link PlaybackOverlayFragment} handles user interaction and interacts with the {@link VdoPlayer}
+ * held by this class.
+ */
+public class TvPlayerActivity extends Activity implements VdoPlayer.InitializationListener,
+        VdoPlayer.PlaybackEventListener {
+    private static final String TAG = "TvPlayerActivity";
+    public static final String OTP = "otp";
+    public static final String PLAYBACK_INFO = "playbackInfo";
+
+    private VdoPlayerFragment playerFragment;
+    private PlaybackOverlayFragment overlayFragment;
+    private VdoPlayer mPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tv_player);
+
+        overlayFragment = (PlaybackOverlayFragment)getFragmentManager().findFragmentById(R.id.tv_player_overlay_fragment);
+        playerFragment = (VdoPlayerFragment)getFragmentManager().findFragmentById(R.id.tv_player_fragment);
+        playerFragment.initialize(TvPlayerActivity.this);
+    }
+
+    @Override
+    public void onInitializationSuccess(VdoPlayer.PlayerHost playerHost, VdoPlayer vdoPlayer, boolean restored) {
+        Log.i(TAG, "init success");
+        mPlayer = vdoPlayer;
+        vdoPlayer.addPlaybackEventListener(TvPlayerActivity.this);
+        loadParams();
+    }
+
+    @Override
+    public void onInitializationFailure(VdoPlayer.PlayerHost playerHost, ErrorDescription errorDescription) {
+        Log.e(TAG, "init failure");
+        showToast("Initialization failure. Reason: " + errorDescription.errorCode + ", " + errorDescription.errorMsg);
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int state) {
+        overlayFragment.playbackStateChanged(playWhenReady, state);
+    }
+
+    @Override
+    public void onSeekTo(long timeMs) {}
+
+    @Override
+    public void onProgress(long timeMs) {
+        overlayFragment.playbackPositionChanged(timeMs);
+    }
+
+    @Override
+    public void onBufferUpdate(long timeMs) {}
+
+    @Override
+    public void onPlaybackSpeedChanged(float speed) {}
+
+    @Override
+    public void onLoading(VdoPlayer.VdoInitParams vdoInitParams) {
+        Log.i(TAG, "onLoading");
+    }
+
+    @Override
+    public void onLoaded(VdoPlayer.VdoInitParams vdoInitParams) {
+        Log.i(TAG, "onLoaded");
+        mPlayer.setPlayWhenReady(true);
+        overlayFragment.playbackDurationChanged(mPlayer.getDuration());
+    }
+
+    @Override
+    public void onLoadError(VdoPlayer.VdoInitParams vdoInitParams, ErrorDescription errorDescription) {
+        showToast("onLoadError " + errorDescription.errorCode + ": " + errorDescription.errorMsg);
+    }
+
+    @Override
+    public void onMediaEnded(VdoPlayer.VdoInitParams vdoInitParams) {
+        Log.i(TAG, "onMediaEnded");
+    }
+
+    @Override
+    public void onError(VdoPlayer.VdoInitParams vdoInitParams, ErrorDescription errorDescription) {
+        showToast("onError code " + errorDescription.errorCode + ": " + errorDescription.errorMsg);
+    }
+
+    @Override
+    public void onTracksChanged(Track[] tracks, Track[] tracks1) {}
+
+    private void loadParams() {
+        Intent intent = getIntent();
+        String otp = intent.getStringExtra(OTP);
+        String playbackInfo = intent.getStringExtra(PLAYBACK_INFO);
+        VdoPlayer.VdoInitParams vdoParams = VdoPlayer.VdoInitParams.createParamsWithOtp(otp, playbackInfo);
+        mPlayer.load(vdoParams);
+    }
+
+    public void setPlayWhenReady(boolean playWhenReady) {
+        if (mPlayer != null) {
+            mPlayer.setPlayWhenReady(playWhenReady);
+        }
+    }
+
+    public void fastForward() {
+        if (mPlayer != null) {
+            long target = Math.min(mPlayer.getDuration(), mPlayer.getCurrentTime() + 5000);
+            mPlayer.seekTo(target);
+        }
+    }
+
+    public void rewind() {
+        if (mPlayer != null) {
+            long target = Math.max(0, mPlayer.getCurrentTime() - 5000);
+            mPlayer.seekTo(target);
+        }
+    }
+
+    private void showToast(String msg) {
+        Log.i(TAG, msg);
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+}
