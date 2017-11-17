86807a5d3d06ed226f701ef407f2847bba6399d5
Vikas <vkq0011@gmail.com>
2017-11-17 19:01:36 +0530

    add some utility methods

--- app/src/main/java/com/vdocipher/sampleapp/Utils.java
+++ app/src/main/java/com/vdocipher/sampleapp/Utils.java
@@ -12,6 +12,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -88,6 +90,19 @@ public class Utils {
                 stateName = "STATE_UNKNOWN";
         }
         return "playWhenReady " + (playWhenReady ? "true" : "false") + ", " + stateName;
+    }
+
+    public static String getSizeString(int bitsPerSec, long millisec) {
+        double sizeMB = ((double)bitsPerSec / (8 * 1024 * 1024)) * (millisec / 1000);
+        return round(sizeMB, 2) + " MB";
+    }
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 
     // call on non-ui thread only
